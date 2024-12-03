824c627f8946bb2a74b4633354ebb94bb2a5a6c1
Muhammad Hamza <hamzakabeer021@gmail.com>
2024-12-03 16:04:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+      const item = header.parentElement;
+  
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        document.querySelectorAll('.accordion-item').forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+      }
+    });
+  });
+  
